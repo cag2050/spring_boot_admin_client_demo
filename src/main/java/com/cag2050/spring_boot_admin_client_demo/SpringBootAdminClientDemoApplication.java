e45a1d82b88cbd2c1abc3f229f1c6d690671e68d
chen_anguo <cag2050@163.com>
2019-08-22 11:00:34 +0800

    project init

---
+++ src/main/java/com/cag2050/spring_boot_admin_client_demo/SpringBootAdminClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cag2050.spring_boot_admin_client_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientDemoApplication.class, args);
+	}
+
+}
